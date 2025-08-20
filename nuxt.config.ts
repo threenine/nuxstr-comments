afc33afefc1ebbc3d69ec817de73e1af0f70eb1b
Gary Woodfine <lnb0l9dc@duck.com>
2025-08-20 09:44:02 +0100

    Add `@nuxt/content` as a devDependency and reformat `nuxt.config.ts`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-08-19',
-  experimental: {nativeSqlite: true},
   modules: [
-    "@nuxt/content",
-    "@nuxt/eslint",
-    "@nuxt/scripts",
-    "@nuxt/test-utils",
-    "@nuxt/ui"
-  ]
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+  ],
+  experimental: { nativeSqlite: true },
+  compatibilityDate: '2025-08-19',
 })

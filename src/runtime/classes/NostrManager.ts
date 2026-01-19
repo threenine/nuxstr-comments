f83f57e42dec2719f8655ca6e1a5946cb243f80b
Gary Woodfine <lnb0l9dc@duck.com>
2026-01-19 09:32:22 +0000

    feat: add initial NostrManager class with basic imports from `nostr-tools`

---
+++ src/runtime/classes/NostrManager.ts
@@ -0,0 +1,13 @@
+import {
+  SimplePool,
+  Filter as NToolFilter,
+  Event as NToolEvent,
+  EventTemplate as NToolEvenTemplate,
+  verifyEvent,
+} from "nostr-tools";
+
+export class NostrManager {
+  constructor() {
+
+  }
+}
